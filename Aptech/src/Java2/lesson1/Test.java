d750edc550c7a976d63ed9b70c437f391213c249
Tien Dat <dattocngan@gmail.com>
2021-03-10 15:08:27 +0700

    add folder Java 2

---
+++ Aptech/src/Java2/lesson1/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java2.lesson1;
+
+/**
+ *
+ * @author MyPC
+ */
+public class Test {
+    public static void main(String[] args) {
+        String a = "Nguyen Tien Dat";
+        System.out.println(a);
+    }
+}
