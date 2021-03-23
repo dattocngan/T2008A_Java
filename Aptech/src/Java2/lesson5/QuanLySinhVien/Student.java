f4c68d62b2930b2ec300e87e87e4c7b47b81deb5
Tien Dat <dattocngan@gmail.com>
2021-03-24 00:10:01 +0700

    add Java

---
+++ Aptech/src/Java2/lesson5/QuanLySinhVien/Student.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java2.lesson5.QuanLySinhVien;
+
+/**
+ *
+ * @author MyPC
+ */
+public class Student {
+    String name, gender, email, phoneNumber;
+    int age;
+
+    public Student() {
+    }
+
+    public Student(String name, String gender, String email, String phoneNumber, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", gender=" + gender + ", email=" + email + ", phoneNumber=" + phoneNumber + ", age=" + age + '}';
+    }
+    
+}
